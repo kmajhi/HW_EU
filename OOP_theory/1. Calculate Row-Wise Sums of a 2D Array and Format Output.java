1038cafde9337ef8cb59ed9686f6f7882b9cad22
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-28 23:37:15 +0600

    1. Calculate Row-Wise Sums of a 2D Array and Format Output.java

--- OOP_theory/1. Calculate Row-Wise Sums of a 2D Array and Format Output.java
+++ OOP_theory/1. Calculate Row-Wise Sums of a 2D Array and Format Output.java
@@ -16,3 +16,34 @@ public class Main {
         System.out.println(" ]");
     }
 }
+
+
+/* Another way
+
+    public class Main {
+    public static void main(String[] args) {
+        int[][] mat1 = {{1, 2, 3}, {1, 6, 3}, {1, 2, 10}};
+
+        for (int i = 0; i < mat1.length; i++) {
+            int sum = 0; // Initialize sum for each row
+            for (int j = 0; j < mat1[i].length; j++) {
+                sum += mat1[i][j]; // Add each element to the sum
+            }
+            System.out.print("[ ");
+            for (int j = 0; j < mat1[i].length; j++) {
+                System.out.print(mat1[i][j]);
+                if (j < mat1[i].length - 1) {
+                    System.out.print(" + ");
+                }
+            }
+            System.out.println(" ] = " + sum);
+        }
+    }
+}
+
+//output 
+[ 1 + 2 + 3 ] = 6
+[ 1 + 6 + 3 ] = 10
+[ 1 + 2 + 10 ] = 13
+
+*/
