9279552b7a4064cbd5540074c10aa6a19dd4fed3
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-09-06 04:08:09 +0600

    indexing array.java

---
+++ OOP_theory/indexing array.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+       int [] primes= {1,2,3,4,5};
+
+       primes[2] = 0;
+       int k = primes[2];
+
+        System.out.println("k = " + k);
+    }
+}
